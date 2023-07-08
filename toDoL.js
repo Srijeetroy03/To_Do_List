c923f7dc35bc5d37806deaa30c57f5ecc42c7edd
Srijeetroy03 <srijeetroy03@gmail.com>
2023-07-09 00:34:53 +0530

    c

--- toDoL.js
+++ toDoL.js
@@ -5,11 +5,19 @@ add.addEventListener('click',()=>{
     let show=document.getElementById('show')
     let text=document.getElementById('task_input');
     let list=document.createElement('li')
-    list.innerHTML=text.value;
-    // list.classList.add('toDo')
-    list.setAttribute('class','toDo')
-    show.appendChild(list)
-    text.value=null
+    let para=document.createElement('p')
+    para.innerHTML=text.value;
+    if(para.innerHTML==""){
+        alert("Add a task!")
+    }
+    else{
+        list.appendChild(para)
+        // list.classList.add('toDo')
+        list.setAttribute('class','toDo')
+        show.appendChild(list)
+        text.value=null
+    }
+    
     let toDo=document.querySelectorAll('.toDo')
     for(let i of toDo){
         i.setAttribute('title','click to delete task')
@@ -22,9 +30,14 @@ add.addEventListener('click',()=>{
             }
         })
     }
+    
 })
 
 r_all.addEventListener('click',()=>{
     let show=document.getElementById('show')
+    if(show.innerHTML==""){
+        alert("You have cleared all tasks")
+    }
+    else
     show.innerHTML=null
 })
